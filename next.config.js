1b262f18a54b19a516b5832f5e7788e390ead0ed
Neher <nehera@msoe.edu>
2023-09-18 21:48:32 -0500

    GitHub action

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const repo = 'comp-sci-canvas';
 
-module.exports = nextConfig;
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'msoe-367267903.imgix.net',
+  },
+};
