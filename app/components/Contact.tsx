d16548781eba344b22266aff4ede71790ce36b0c
nehera21 <113081779+nehera21@users.noreply.github.com>
2024-12-30 14:28:39 -0600

    Update Contact.tsx

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
-import { SocialIcon } from 'react-social-icons';
-import { AnimatedBox } from './AnimatedBox';
+import { Box, Flex } from '@chakra-ui/react'
+import { SocialIcon } from 'react-social-icons'
+import { colors } from '../Providers'
+import { AnimatedBox } from './AnimatedBox'
 
 export type ContactProps = {
-  links: string[];
-};
+    links: string[]
+}
 export default function Contact(props: ContactProps) {
-  return (
-    <Flex pl="5">
-      {props.links.map((link, index) => {
-        return (
-          <Box key={index} mr="15px">
-            <AnimatedBox whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.98 }}>
-              <SocialIcon url={link} />
-            </AnimatedBox>
-          </Box>
-        );
-      })}
-    </Flex>
-  );
+    return (
+        <Flex pl='5'>
+            {props.links.map((link) => { 
+                return (
+                    <Box mr='15px'>
+                        <AnimatedBox
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.98 }}
+                        >
+                            <SocialIcon 
+                                url={link}
+                            />
+                        </AnimatedBox>
+                    </Box>
+                )
+            })}
+        </Flex>
+    )
 }
