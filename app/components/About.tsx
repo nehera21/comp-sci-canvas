9ba0322d4196c178b2a33fb54c59ecf18110f34f
nehera21 <113081779+nehera21@users.noreply.github.com>
2024-12-30 14:28:06 -0600

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,17 +1,28 @@
-'use client';
+'use client'
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react'
 
 export type AboutProps = {
-  text: string;
-};
+    text: string
+}
+
 export default function About(props: AboutProps) {
   const { text } = props;
   return (
-    <Box id="about">
-      <Text fontSize="18" fontFamily="mono" mt="15px" mr="45px" ml="35px">
+    <Box 
+      id='about'
+      w='100%'  // Take full width of parent
+      maxW='800px'  // Maximum width on larger screens
+      mx='auto'  // Center the box
+    >
+      <Text 
+        fontSize={['16px', '18px']}  // Responsive font size
+        fontFamily='mono' 
+        p={['15px', '15px 35px']}  // Responsive padding
+        textAlign='justify'
+      >
         {text}
       </Text>
     </Box>
-  );
+  )
 }
