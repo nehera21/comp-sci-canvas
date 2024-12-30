ddf71853add5a4d84ef352131c52c067fd5fd1a3
nehera21 <113081779+nehera21@users.noreply.github.com>
2024-12-30 15:25:33 -0600

    Update Menu.tsx

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import MenuItem from './MenuItem';
-import { Box } from '@chakra-ui/react';
+import MenuItem from "./MenuItem";
+import { Box, Heading } from "@chakra-ui/react";
 
-type MenuItemType = {
-  name: string;
-  jumpID: string;
-};
-type MenuProps = {
-  menuItems: MenuItemType[];
-};
-export default function Menu({ menuItems }: MenuProps) {
-  return (
-    <Box pl="6" width="600px" justifyContent="right">
-      {menuItems.map((item) => (
-        <MenuItem key={item.jumpID} {...item} />
-      ))}
-    </Box>
-  );
+export default function Menu(props: any) {
+    return (
+        <Box pl='6' width='600px' justifyContent='right'>
+            {props.menuItems.map((item: any) => <MenuItem {...item} />)}
+        </Box>
+    )
 }
