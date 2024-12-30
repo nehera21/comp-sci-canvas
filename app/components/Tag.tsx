ccabfd84b0305c99592fd7f2b18229833a178058
nehera21 <113081779+nehera21@users.noreply.github.com>
2024-12-30 15:27:45 -0600

    Update Tag.tsx

--- app/components/Tag.tsx
+++ app/components/Tag.tsx
@@ -1,46 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { Text } from '@chakra-ui/react';
+import React from 'react';
+import { Text } from '@chakra-ui/react'
 import { AnimatedBox } from './AnimatedBox';
 
 export type TagProps = {
-  name: string;
-  link: string;
-  isLink: boolean;
-};
-
+    name: string,
+    link: string,
+    isLink: boolean
+}
 export default function Tag(props: TagProps) {
-  const { name, link, isLink } = props;
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  return (
-    <AnimatedBox
-      bgColor="brand.accent"
-      borderRadius={'15px'}
-      pl="2"
-      pr="2"
-      ml="2"
-      mr="2"
-      whileHover={{ backgroundColor: '#FFEC96' }}
-      whileTap={{ backgroundColor: '#FFFFE0', scale: 0.98 }}
-    >
-      {isClient && isLink ? (
-        <Text
-          as="a"
-          href={link}
-          cursor="pointer"
-          color="#333333"
-          target="_blank"
-          rel="noopener noreferrer"
+    const {name, link, isLink} = props;
+    return (
+        <AnimatedBox
+            bgColor='brand.accent'
+            borderRadius={'15px'}
+            pl='2'
+            pr='2'
+            mr='2'
+            whileHover={{ backgroundColor: '#FFEC96' }}
+            whileTap={{ backgroundColor: '#FFFFE0', scale: 0.98 }}
         >
-          {name}
-        </Text>
-      ) : (
-        <Text color="#333333">{name}</Text>
-      )}
-    </AnimatedBox>
-  );
+            { isLink && (
+                <Text as="span" onClick={() => window.location.href = link} cursor="pointer" color='#333333'>
+                    {name}
+                </Text>
+            )}
+            { !isLink && (
+                <Text color='#333333'>
+                    {name}
+                </Text> 
+            )}
+        </AnimatedBox>
+    )
 }
